76d589b2ec48ab6fc05d558b344af07c5c37a04e
unknown <rajan23024@gmail.com>
2023-05-18 16:45:14 +0530

    initial commit

---
+++ dist/controllers/user.controller.js
@@ -0,0 +1,10 @@
+"use strict";
+// Uncomment these imports to begin using these cool features!
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UserController = void 0;
+// import {inject} from '@loopback/core';
+class UserController {
+    constructor() { }
+}
+exports.UserController = UserController;
+//# sourceMappingURL=user.controller.js.map
